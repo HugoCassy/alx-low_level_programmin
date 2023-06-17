971cd7dbe507bf2eda1e7ddb83104e0d4a29c7f5
HugoCassy <kasmirxxhris@gmail.com>
2023-06-17 11:21:26 +0000

    Small and Cap letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints capital and small letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char sL;
+	char cL;
+
+	for (sL = 'a'; sL <= 'z'; sL++)
+	{
+		putchar(sL);
+
+		for (cL = 'A'; cL <= 'Z'; cL++)
+		{
+			putchar(cL);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
+
