f49f6f14921fd0d57e5e9929ad70db915651645d
HugoCassy <kasmirxxhris@gmail.com>
2023-06-17 10:52:01 +0000

    ALpjabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints all the alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz");
+	return (0);
+}
