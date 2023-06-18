0298247278faa33fdf224cec518791529deaffdc
HugoCassy <kasmirxxhris@gmail.com>
2023-06-18 20:22:36 +0000

    Erro statement

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main()
 {
-	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, strlen(message));
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
 	return (1);
 }
