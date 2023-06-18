d636d263031f6d855fa9b5ed4165ccd2597c0371
HugoCassy <kasmirxxhris@gmail.com>
2023-06-18 05:40:38 +0000

    One to hundred

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints 1 to 100
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		putchar(i + '0');
+	}
+	return (0);
+}
