1b8f1517a226d9d20084a1f2527c6246ea64267e
HugoCassy <kasmirxxhris@gmail.com>
2023-06-18 17:18:28 +0000

    COmbo of 2letters

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Prints all combination of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int first, second;
+
+	for (first = 0; first <= 9; first++)
+	{
+		for (second = first + 1; second <= 9; second++)
+		{
+			if (first != second)
+			{
+				putchar(first + '0');
+				putchar(second + '0');
+
+				if (first != 8 || second != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+}
