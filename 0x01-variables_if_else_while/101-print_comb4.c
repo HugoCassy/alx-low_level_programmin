4d1d200071fa83a8c7f6968f903d9ee6128dea7c
HugoCassy <kasmirxxhris@gmail.com>
2023-06-18 18:12:18 +0000

    3 digit numbers in mix

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Prints 3 digits combos
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j, k, l;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (k = j + 1; k <= 9; k++)
+		{
+			for (l = k + 1; l <= 9; l++)
+			{
+				if ((j != k || l) || (k != j || l) || (l != k || j))
+				{
+					putchar(j + '0');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (j != 7 || k != 8 || j != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
