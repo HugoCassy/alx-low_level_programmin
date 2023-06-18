a9a5b3eee5cee4466fb9087fcb46862ba01820ad
HugoCassy <kasmirxxhris@gmail.com>
2023-06-18 05:58:50 +0000

    Numbers then a ,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,15 +9,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
-		if (i == 8)
-		{
-			putchar('9');
-		}
 	}
+		putchar('9');
 	return (0);
 }
