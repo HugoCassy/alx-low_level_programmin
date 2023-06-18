e417e51a70a9f10a4cf4ec7aedb94a6bf9307d75
HugoCassy <kasmirxxhris@gmail.com>
2023-06-18 19:32:05 +0000

    Combined numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Prints combos for numbers
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
