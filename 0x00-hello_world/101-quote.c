d781d233cfc31056d1e9dbe81c3cc36fd40858fe
HugoCassy <kasmirxxhris@gmail.com>
2023-06-19 21:17:05 +0000

    Strings in C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,12 @@
  */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, 59);
+	char message[66] = "and that piece of art is";
+	char message_2[] = " useful\" - Dora Korpar, 2015-10-19\n";
+
+	strcat(message, message_2);
+	size_t length = strlen(message);
+
+	write(2, message, length);
 	return (1);
 }
