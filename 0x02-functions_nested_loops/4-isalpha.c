3a866922afefaf8b8ce2d674354d130cd915634b
HugoCassy <kasmirxxhris@gmail.com>
2023-06-21 11:30:44 +0000

    Any letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower - Checks for lowers
+ *@r:  integer value that will be used to compare
+ *Return: Always 0 (Success)
+ */
+int _islower(int r)
+{
+	if ((r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z'))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+	putchar('\n');
+}
