30882da735275c77eaff565a8163eb873cc0df3e
HugoCassy <kasmirxxhris@gmail.com>
2023-06-21 10:44:23 +0000

    message

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,8 +4,10 @@
  * @r:  integer value that will be used to compare
  * Return: Always 0 (Success)
  */
-int _islower(int r)
+int _islower()
 {
+	int r;
+
 	if (r >= 96 && r <= 122)
 	{
 		_putchar(1);
