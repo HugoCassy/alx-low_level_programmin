9c1e6247756ffd41e6a41c355163ecf9e043863c
HugoCassy <kasmirxxhris@gmail.com>
2023-06-21 10:07:40 +0000

    3-islower.c edited again

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
  * _islower - Checks for lowers
- *
- * r: integer value that will be used to compare
+ * @r:  integer value that will be used to compare
  * Return: Always 0 (Success)
- *
  */
 int _islower(int r)
 {
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (r);
+	if (r >= 96 && r <= 122)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
 }
