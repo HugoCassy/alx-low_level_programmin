79cd2ff1241d05baaa40c059c5b515cb23f61b1a
HugoCassy <kasmirxxhris@gmail.com>
2023-06-22 01:05:45 +0000

    9times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,15 +6,17 @@
  */
 void times_table(void)
 {
-	int j,k;
+	int j, k;
 
 	for (j = 0; j <= 9; j++)
 	{
 		for (k = 0; k <= 9; k++)
 		{
 			int result = k * j;
+
 			_putchar (result + '0');
 			_putchar (',');
+			_putchar (' ');
 		}
 		_putchar ('\n');
 	}
