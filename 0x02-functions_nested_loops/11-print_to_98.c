8375e4c335b470a8cf7b215152451039a28d5056
HugoCassy <kasmirxxhris@gmail.com>
2023-06-22 00:58:39 +0000

    Naturaks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,10 +6,9 @@
  */
 void print_to_98(int n)
 {
-
 	if (n == 98)
 	{
-		printf("%d", n);
+		printf("%d\n", n);
 	}
 	else if (n < 98)
 	{
@@ -19,10 +18,25 @@ void print_to_98(int n)
 			if (n != 98)
 			{
 				putchar (',');
+				putchar (' ');
 			}
 		}
 		putchar('\n');
 	}
+	else
+	{
+		for (n = n; n <= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+		putchar ('\n');
+	}
+
 
 
 }
