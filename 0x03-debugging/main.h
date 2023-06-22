44c3d7a1293cb1e2c958f150280cd7f49ac19c26
HugoCassy <kasmirxxhris@gmail.com>
2023-06-22 17:43:43 +0000

    Leaps

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -16,5 +16,6 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 int positive_or_negative(int n);
-int largest_number(int, int, int);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int a, int b, int c);
 #endif
