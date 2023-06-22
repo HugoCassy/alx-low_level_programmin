b1e7d19589c72de1f5e5fc1cfb1ce943c4bb7523
HugoCassy <kasmirxxhris@gmail.com>
2023-06-22 13:25:51 +0000

    Postive or negative

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory explains how i debug alx files
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,19 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+
+int _putchar(int c);
+int print_alphabet(void);
+int print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha();
+int print_sign();
+int _abs(int a);
+int print_last_digit(void);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+int positive_or_negative(int n);
+#endif
