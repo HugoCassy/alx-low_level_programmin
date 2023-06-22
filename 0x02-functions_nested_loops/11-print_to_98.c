df8429c9758e89bb7f928a5eb9274be1778bc50c
HugoCassy <kasmirxxhris@gmail.com>
2023-06-22 00:50:57 +0000

    :naturals

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,24 @@
  */
 void print_to_98(int n)
 {
-	printf("%d", n);
-	putchar ('\n');
+
+	if (n == 98)
+	{
+		printf("%d", n);
+	}
+	else if (n < 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				putchar (',');
+			}
+		}
+		putchar('\n');
+	}
+
+
 }
 
