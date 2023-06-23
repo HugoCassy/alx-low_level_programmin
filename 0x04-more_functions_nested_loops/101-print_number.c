d0ff209efd67b6bac7cb3ed874f18da781f225ca
HugoCassy <kasmirxxhris@gmail.com>
2023-06-23 17:16:31 +0000

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @c: holds the integer
+ * Return: 0
+ */
+void print_number(int c)
+{
+	if (c < 0)
+	{
+		_putchar ('-');
+		c = -c;
+	}
+
+	int line = 1;
+
+	while (c / line >= 10)
+	{
+		line = line * 10;
+	}
+	while (line > 0)
+	{
+		int number = c / line;
+
+		_putchar (number + '0');
+		c %= line;
+		line /= 10;
+	}
+}
