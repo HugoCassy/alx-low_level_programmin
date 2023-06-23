efe3b6db0dde1e51efb0c7d2a6556b5bd1d1e98a
HugoCassy <kasmirxxhris@gmail.com>
2023-06-23 02:49:30 +0000

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - prints a long line
+ * @n: line hold value
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int b;
+
+	if (n > 0)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			_putchar ('_');
+		}
+		_putchar ('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
