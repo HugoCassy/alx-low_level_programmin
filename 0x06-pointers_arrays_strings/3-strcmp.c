a4faaab787af35acb0c9f1c0b349dac3d3620d06
HugoCassy <kasmirxxhris@gmail.com>
2023-06-30 00:42:07 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,18 +5,27 @@
  * @s2: string two
  * Return: negative or posit
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			return s1[i] - s2[i];
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
+	if (s1[i] != '\0')
+	{
+		return (1);
+	}
+	else if (s2[i] != '\0')
+	{
+		return (-1);
+	}
+
+	return (0);
 }
 
