9bf99d7a42b119ba77d4bc905d4b7b82cb77eeb4
HugoCassy <kasmirxxhris@gmail.com>
2023-07-03 23:30:28 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strchr - locates the first apperance of a string
+ * @s: array of letters
+ * @c: letter to be located
+ * Return: c
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return NULL;
+}
