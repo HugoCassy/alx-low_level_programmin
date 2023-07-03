7358d1ce5a88ffe0606952327baefa21a9e7680d
HugoCassy <kasmirxxhris@gmail.com>
2023-07-03 23:51:30 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * int _strspn - gets the length of a prefix
+ * @s: main long string
+ * @accept:substring
+ * Return: s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
