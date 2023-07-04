b4220671447d184f0ac88d345579df79e291d358
HugoCassy <kasmirxxhris@gmail.com>
2023-07-04 00:12:52 +0000

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,4 +7,30 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	int len = 0;
 
+	int seen;
+
+	while (*s != '\0')
+	{
+
+		seen = 0;
+
+		for (char *p = accept; *p != '\0'; p++)
+		{
+			if (*s == *p)
+			{
+				seen = 1;
+				break;
+			}
+		}
+		if (seen == 0)
+		{
+			return (len);
+		}
+
+		len++;
+		s++;
+	}
+	return (len);
+}
