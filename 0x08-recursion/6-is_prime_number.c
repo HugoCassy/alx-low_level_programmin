e799874677c284b8539d717cd3e8497177aff8f8
HugoCassy <kasmirxxhris@gmail.com>
2023-07-06 15:23:09 +0000

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,16 +6,12 @@
  */
 int is_prime_number(int n)
 {
-	if (n <= 0 && n / 2 != 0)
+	if (n / 2 == 0)
+	{
+		return (0);
+	}
+	else
 	{
 		return (1);
-	}
-	else if (n >= 0 && n / 2 != 0)
-	{
-		return (0);
-	}
-	else 
-	{
-		return (0);
 	}
 }
