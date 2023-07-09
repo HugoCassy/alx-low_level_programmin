f7dfe9253df4c46720764de28461bf4b4e6835fd
HugoCassy <kasmirxxhris@gmail.com>
2023-07-09 23:37:20 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - prints that prints its name then a newline
+ * @agrc: number of arguments passed to the function
+ * @agrv: Array of the arguments passed.
+ * Return: 0;
+ */
+int main(int agrc, char *agrv[])
+{
+	printf("%s", agrv[0]);
+}
