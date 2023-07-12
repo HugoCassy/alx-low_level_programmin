8a9be61350ad46bae75176de57c69dd75275aad4
HugoCassy <kasmirxxhris@gmail.com>
2023-07-12 16:00:09 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,6 +27,9 @@ size_t _str_length(const char *str)
  */
 char *str_concat(char *s1, char *s2)
 {
+	size_t len1, len2, i, j;
+	char *conca_t;
+
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -34,16 +37,10 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 	{
 		s2 = "";
-	}
-	size_t len1; 
+	} 
 
 	len1 = _str_length(s1);
-
-	size_t len2;
-
 	len2 = _str_length(s2);
-
-	char *conca_t;
 
 	conca_t = malloc((len1 + len2 + 1) * sizeof(char));
 
@@ -51,9 +48,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-
-	size_t i, j;
-
 	for (i = 0; i < len1; i++)
 	{
 		conca_t[i] = s1[i];
@@ -63,7 +57,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		conca_t[i + j] = s2[j];
 	}
-
 	conca_t[i + j] = '\0';
 	return (conca_t);
 }
