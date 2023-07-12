161b23bec4b069350f801bc802d7659b30589e7c
HugoCassy <kasmirxxhris@gmail.com>
2023-07-12 19:51:18 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,26 +1,42 @@
 #include "main.h"
 /**
- * alloc_grid - Allocates a 2D array of integers.
+ * free_grid - Frees the memory of a 2D array.
+ * @grid: The grid to free.
+ * @height: The height of the grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
+
+/**
+ * alloc_grid - Creates a 2D array of integers.
  * @width: The width of the grid.
  * @height: The height of the grid.
- * Return: On success, returns a pointer to the allocated 2D array.
- * On failure or if width/height is zero or negative, returns NULL.
+ * Return: On success, returns a pointer to the created 2D array.
+ * On failure or if width/height is 0 or negative, returns NULL.
  */
 int **alloc_grid(int width, int height)
 {
-	int **grid, i;
-
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	grid = malloc(height * sizeof(int *));
+	int **grid = malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 	{
 		return (NULL);
 	}
+
+	int i;
 
 	for (i = 0; i < height; i++)
 	{
@@ -32,5 +48,25 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
+
 	return (grid);
+}
+
+/**
+ * initialize_grid - Initializes a 2D array of integers with 0.
+ * @grid: The grid to initialize.
+ * @width: The width of the grid.
+ * @height: The height of the grid.
+ */
+void initialize_grid(int **grid, int width, int height)
+{
+	int i, j;
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
 }
