4dd997067867617415be385f59be698c2934a846
HugoCassy <kasmirxxhris@gmail.com>
2023-07-12 17:08:32 +0000

    4-free_grid.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,11 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 int _putchar(int c);
 int print_alphabet(void);
 int print_alphabet_x10(void);
